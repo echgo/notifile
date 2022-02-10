d8cfe72af0c7cbee9b29d33ae0e354b332a8c807
Jonas Kwiedor <jonas.kwiedor@jj-ideenschmiede.de>
2022-02-10 16:17:14 +0100

    Add test function

--- notifile_test.go
+++ notifile_test.go
@@ -1,1 +1,19 @@
 package notifile
+
+import (
+	"log"
+	"testing"
+)
+
+// TestCreate is to create a new notification file
+func TestCreate(t *testing.T) {
+	data := Data{
+		Channel:  []string{"gotify"},
+		Headline: "J&J Afterialo",
+		Message:  "Bitte schau doch mal nach.",
+	}
+	err := Create(data, "/var/lib/echgo/notification")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
